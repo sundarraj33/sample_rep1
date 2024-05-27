5ba72e912a837208602a2c07153c463b2112afb4
sundarraj33 <sundarraj.m033@gmail.com>
2024-05-27 14:56:43 +0530

    add1.js File Created sample page

--- add1.js
+++ add1.js
@@ -1,18 +1,5 @@
 function addfun(){
-    res.write(`<html>
-                <head></head>
-                <body>
-                <h2>Add Page in Node</h2>
-                </body>
-                </html>
-    `);
-    res.end();
-    
-    // console.log('Add page');    
-    
-    
+        alert('Sample Add JS File')    ;
 }
 
 addfun();
-
-module.exports = {addfun,text:'Sample page in console'};
